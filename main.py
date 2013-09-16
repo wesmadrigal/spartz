d72fcec98bebaa92ecaac0d6f4c691a3acd1b912
Wes <wes@wes.(none)>
2013-09-16 16:00:28 -0500

    csv version re-added

--- main.py
+++ main.py
@@ -97,7 +97,89 @@ def store_data_db(path):
  
         else:
             print "Hit an error and missed %s" % _set[0]
-    print "Finished writing file"
+    print "Finished"
+
+
+# depending on your needs
+def store_data_csv(path):
+    global api
+    data = format_data(path) 
+    # for the latitude and longitude coords
+    sequence = re.compile(r'[0-9]+[.][0-9]+')
+
+    with open('data.csv', 'wb') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',') 
+        for _set in data[1:]: 
+            # need id, fbid, name, lat, lon, continent, country, province, city
+            d = _set[0].split(',')
+            # data is in the form '"data"' so we must handle the erroneous string denotation
+            _id = d[0].strip('"')
+            fbid = d[1].strip('"')
+            if len(d) == 5:
+                name = d[2].strip('"')
+            elif len(d) == 6:
+                one = d[2].strip('"')
+                two = d[3].strip('"')
+                name = ','.join( [one, two] )
+                name = name.strip('"')
+
+            coords = [ i for i in d if len(re.findall(sequence, i)) > 0 ]
+            if len(coords) > 1:
+                lat, lon = coords[0].strip('"'), coords[1].strip('"')
+                formatted = api.format(lat, lon)
+                res = urllib2.urlopen(formatted).read()
+                res = json.loads(res)
+                country_code = res['address']['country_code'].upper()
+
+                try:
+                    country = countries[country_code]['name']
+                except KeyError:
+                    if 'country' in res['address'].keys():
+                        country = res['address']['country']
+                    else:
+                        country = 'NULL'
+
+                if 'continent' in res['address'].keys():
+                    continent = res['address']['continent']
+                else:
+                    conts = []
+                    for cont in continents.keys():
+                        for count in continents[cont]['countries']:
+                            if country == countries[count]['name']:
+                                conts.append(cont)
+                    if len(conts) > 0:
+                        continent = continents[conts[0]]['name']
+                    else:
+                        continent = 'NULL'
+
+                if 'state' in res['address'].keys():
+                    state_province = res['address']['state']
+                else:
+                    #print "state not an available key"
+                    state_province = 'NULL'
+ 
+                if 'town' in res['address'].keys():
+                    city = res['address']['town']
+                elif 'city' in res['address'].keys():
+                    #print "city instead of town"
+                    city = res['address']['city']
+                else:
+                    city = 'NULL'
+
+                # handle unicode errors
+                # difference encodings from csv and api hence the inconsistent encoding/decoding methods                
+                facebook_data = [ _id, fbid, name, lat, lon ]
+                facebook = [ i.decode('latin-1').encode('utf-8') for i in facebook_data ]
+                api_data = [ continent, country, state_province, city ]
+                encoded = [ i.encode('utf-8') for i in api_data ]
+                row = facebook + encoded
+                updated = int(time.time())
+                row += [updated]
+                writer.writerow(row)
+ 
+            else:
+                print "Hit an error and missed %s" % _set[0]
+    print "Finished"
 
 
 def format_data(path):
@@ -111,10 +193,14 @@ def format_data(path):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 3:
         arg = sys.argv[1]
-        os.system("sqlite3 schema.db < schema.sql")
-        store_data_db(arg)
+        _type = sys.argv[2]
+        if _type.startswith('c'):
+            store_data_csv(arg)
+        elif _type.startswith('d'):
+            os.system("sqlite3 schema.db < schema.sql")
+            store_data_db(arg)
     else:
-        print "Takes one path to a csv file as arguments"
+        print "Takes one path to a csv file as argument and csv or db"
 
